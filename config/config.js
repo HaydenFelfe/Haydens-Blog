8861136911e4bd9eae6f36e9aac42d6ff1ba247a
haydenfelfe <felfehayden@gmail.com>
2023-09-17 19:18:20 -0700

    e

--- config/config.js
+++ config/config.js
@@ -1,13 +1,18 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Heroku MySQL database URL
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Local MySQL database configuration
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
